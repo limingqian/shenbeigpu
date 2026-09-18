605d403a59912d0f2a2d57f7726729a8441d4318
mingqian.li <abc350082039@qq.com>
2026-09-18 10:19:15 +0800

    fix: 解决首次进入首页白屏的问题

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ const routes = [
   {
     path: '/',
     name: 'layout',
+    redirect: '/shenbeigpuai/control/home',
     component: () => import('../views/control/sysframe/index.vue'),
     children: [
       {
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  // 部署到 nginx 站点根目录的子路径 /shenbeigpu/ 下时，
+  // 需要设置 publicPath 让打包后的资源 url 自动加上此前缀
+  // publicPath: '/shenbeigpu/'
 }
